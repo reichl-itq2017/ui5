661e0e71ea1bc4163103907f02109af957445d67
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-11 10:43:32 +0200

    XMLView eingeführt

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 sap.ui.define([
-	"sap/m/Text",
-	"sap/m/Link"
-], function (Text, Link) {
+	"sap/ui/core/mvc/XMLView"
+], function (XMLView) {
 	"use strict";
 
-	new Text({
-		text: "Hello World"
-	}).placeAt("content");
+	XMLView.create({
+		viewName: "hts.itq2017.walkthrough.view.App"
+	}).then(function (oView) {
+		oView.placeAt("content");
+	});
+	
+	// new Text({
+	// 	text: "Hello World"
+	// }).placeAt("content");
 
-	new Link({
-		text: "Ich bin der Link zu Google",
-		href: "http://www.google.de/"
-	}).placeAt("content");
+	// new Link({
+	// 	text: "Ich bin der Link zu Google",
+	// 	href: "http://www.google.de/"
+	// }).placeAt("content");
 
-	new Text({
-		text: "Hello Google"
-	}).placeAt("content");
+	// new Text({
+	// 	text: "Hello Google"
+	// }).placeAt("content");
 });
