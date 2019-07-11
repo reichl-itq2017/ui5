c72bc20ffbb22b8a370008ec372815b00250f8cd
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-11 08:47:50 +0200

    Diskussion Donnerstag früh

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 sap.ui.define([
-	"sap/m/Text"
-
-], function (Text) {
+	"sap/m/Text",
+	"sap/m/Link"
+], function (Text, Link) {
 	"use strict";
 
 	new Text({
 		text: "Hello World"
 	}).placeAt("content");
 
+	new Link({
+		text: "Ich bin der Link zu Google",
+		href: "http://www.google.de/"
+	}).placeAt("content");
+
+	new Text({
+		text: "Hello Google"
+	}).placeAt("content");
 });
