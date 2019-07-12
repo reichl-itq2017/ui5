b98f310cbb52e2df34811c931306d6e23d3d6a05
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-12 09:00:26 +0200

    Step 7 JSON Model

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -5,7 +5,17 @@ sap.ui.define([
 ], function (Controller, MessageToast, MessageBox) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-        onShowHello: function () {
+        onInit : function () {
+            // set data model on view
+            var oData = {
+               recipient : {
+                  name : "Welt"
+               }
+            };
+            var oModel = new JSONModel(oData);
+            this.getView().setModel(oModel);
+         },
+         onShowHello: function () {
             // show a native JavaScript alert
             // alert("Alert, alert!");
             MessageToast.show("Hier ist ein Toast...");
