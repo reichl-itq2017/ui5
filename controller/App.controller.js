6d84df95920177e83d8414c29559b796f4952bc4
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-12 08:20:56 +0200

    UI5-Alert eingebaut

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,13 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
         onShowHello: function () {
             // show a native JavaScript alert
             // alert("Alert, alert!");
             MessageToast.show("Hier ist ein Toast...");
+        },
+        onAlert: function () {
+            MessageBox.alert("Alert, alert!", {
+                title: "Alert"
+            });
         }
     });
 });
