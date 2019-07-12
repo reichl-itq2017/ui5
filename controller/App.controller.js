7258f99450a613f2325c7d3651c457408252f6b6
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-12 10:12:36 +0200

    i18n eingeführt

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -2,25 +2,32 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, MessageBox, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, MessageBox, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-        onInit : function () {
+        onInit: function () {
             // set data model on view
             var oData = {
-               recipient : {
-                  name : "Welt"
-               },
-               schüler : {
-                   name: "Hugo",
-                   noten: [1,2,2,1]
-               }
+                recipient: {
+                    name: "Welt"
+                },
+                schüler: {
+                    name: "Hugo",
+                    noten: [1, 2, 2, 1]
+                }
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
-         },
-         onShowHello: function () {
+
+            // set i18n model on view
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");     // Beachte das "i18n"!
+        },
+        onShowHello: function () {
             // show a native JavaScript alert
             // alert("Alert, alert!");
             MessageToast.show("Hier ist ein Toast...");
