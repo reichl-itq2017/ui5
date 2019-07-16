39532d62de491a0d263bb4e47fc0af5670c56cc3
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-16 14:47:23 +0200

    metadata auf manifest:"json" umgestellt

--- Component.js
+++ Component.js
@@ -1,35 +1,30 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
- ], function (UIComponent, JSONModel, ResourceModel) {
-    "use strict";
-    return UIComponent.extend("hts.itq2017.walkthrough.Component", {
-       metadata : {
-          rootView: {
-             "viewName": "hts.itq2017.walkthrough.view.App",
-             "type": "XML",
-             "async": true,
-             "id": "app"
-          }
-       },
-       init : function () {
-          // call the init function of the parent
-          UIComponent.prototype.init.apply(this, arguments);
-          // set data model
-          var oData = {
-             recipient : {
-                name : "Welt"
-             }
-          };
-          var oModel = new JSONModel(oData);
-          this.setModel(oModel);
- 
-          // set i18n model
-          var i18nModel = new ResourceModel({
-             bundleName : "hts.itq2017.walkthrough.i18n.i18n"
-          });
-          this.setModel(i18nModel, "i18n");
-       }
-    });
- });
+   "sap/ui/core/UIComponent",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+   "use strict";
+   return UIComponent.extend("hts.itq2017.walkthrough.Component", {
+      metadata: {
+         manifest: "json"
+      },
+      init: function () {
+         // call the init function of the parent
+         UIComponent.prototype.init.apply(this, arguments);
+         // set data model
+         var oData = {
+            recipient: {
+               name: "Welt"
+            }
+         };
+         var oModel = new JSONModel(oData);
+         this.setModel(oModel);
+
+         // set i18n model
+         var i18nModel = new ResourceModel({
+            bundleName: "hts.itq2017.walkthrough.i18n.i18n"
+         });
+         this.setModel(i18nModel, "i18n");
+      }
+   });
+});
