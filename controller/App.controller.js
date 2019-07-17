590a89619339292d0ed24fc16e63058892affeaa
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-17 11:36:47 +0200

    App.controller aufgeräumt

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,12 +1,8 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/m/MessageBox",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, MessageBox, JSONModel, ResourceModel) {
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-        
+
     });
 });
