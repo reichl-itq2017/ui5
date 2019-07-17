51d6769cec99487ce6f9ca71154477b2ee7a6600
Florian Reichl <reichl-itq2017@cloud.bs-hassfurt.de>
2019-07-17 11:35:19 +0200

    Nested Views

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -7,18 +7,6 @@ sap.ui.define([
 ], function (Controller, MessageToast, MessageBox, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("hts.itq2017.walkthrough.controller.App", {
-        onShowHello: function () {
-            // read msg from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            // show message
-            MessageToast.show(sMsg);
-        },
-        onAlert: function () {
-            MessageBox.alert("Alert, alert!", {
-                title: "Alert"
-            });
-        }
+        
     });
 });
